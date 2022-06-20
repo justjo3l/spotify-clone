3a33906eab6f9f4d71750e912b6363e719697dfd
justjo3l <comedyandcomputers@gmail.com>
2022-06-20 14:58:26 +1000

    Update Center.tsx

--- components/Center.tsx
+++ components/Center.tsx
@@ -43,7 +43,7 @@ function Center() {
   return (
     <div className="flex-grow col-span-full h-screen overflow-y-scroll scrollbar-hide">
         <header className="absolute top-5 right-8">
-            <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}>
+            <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut as any}>
                 <img className="rounded-full w-10 h-10" src={session?.user?.image as string} alt="" />
                 <h2>{session?.user?.name}</h2>
                 <ChevronDownIcon className="h-5 w-5"/>
