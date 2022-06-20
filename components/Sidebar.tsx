047e81fdac185f988da0a0d00f06061f549b0feb
justjo3l <comedyandcomputers@gmail.com>
2022-06-20 14:56:26 +1000

    Added Songs component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,7 +5,6 @@ import {
   HeartIcon,
   RssIcon,
   PlusCircleIcon,
-  LogoutIcon,
 } from "@heroicons/react/outline"
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -32,10 +31,6 @@ function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
         <div className="space-y-4">
-          <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
-            <LogoutIcon className="h-5 w-5"/>
-            <p>Log Out</p>
-          </button>
           <button className="flex items-center space-x-2 hover:text-white">
             <HomeIcon className="h-5 w-5"/>
             <p>Home</p>
--- components/Songs.tsx
+++ components/Songs.tsx
@@ -1,6 +1,16 @@
+import { useRecoilValue } from "recoil";
+import { playlistState } from "../atoms/playlistAtom"
+import Song from "./Song";
+
 function Songs() {
+  const playlist:any = useRecoilValue(playlistState);
+
   return (
-    <div>Songs</div>
+    <div className="px-8 flex flex-col space-y-1 pb-28 text-white">
+      {playlist?.tracks.items.map((track:any, i:any) => (
+        <Song key={track.track.id} track={track} order={i}/>
+      ))}
+    </div>
   )
 }
 
